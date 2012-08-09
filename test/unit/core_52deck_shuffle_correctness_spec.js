1e47d375e3d9f99d2a57977dce94ac67d8fa006b
Jean-Bernard Jansen <jeanbernard.jansen@gmail.com>
2012-08-09 18:05:53 +0200

    Jasmine test updated.

--- test/unit/core_52deck_shuffle_correctness_spec.js
+++ test/unit/core_52deck_shuffle_correctness_spec.js
@@ -1,10 +1,24 @@
-require(['src/core/core'],function(undercover)
-{
-//var random = require('lib/random');
+define(['src/core/core','src/core/random'],function(mod_core,mod_random){
+	underscore = mod_core;
+	Random = mod_random;
 
-	describe('Shuffle for 1000 times', function() {
-		it('Shoud print a shuffle', function() {
+	describe('Shuffle for a lot of times', function() {
+				
+		var rand = Random(Math.round((new Date()).getTime()));
+		var i = 0;
+		var sum = 0;
+		var mean = 0;
+		var iter = 1e7;
+
+		it('should give the same mean each.', function() {
+			for(i = 0; i < iter; i++) {
+				sum = sum + rand();
+			}
+			mean = sum/iter;
 			
-		}); 
+			expect(Math.abs(mean-0.5)).toBeLessThan(1e-4);
+		});
 	});
+
+	return {};
 });
