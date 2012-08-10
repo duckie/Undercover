c3376483783654c2f59321e2dac580d6d61cd52e
Jean-Bernard Jansen <jeanbernard.jansen@gmail.com>
2012-08-10 16:24:26 +0200

    Tests can be ran into Node with a trick with RequireJS for Jasmine to work with.

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
-# Basic readme
+# Undercover Poker Engine
 
+This project is in early development.
+
+## How to run the tests
+
+### With Node.js
+
+Execute the following commands into your terminal:
+
+```bash
+	npm install jasmine-node requirejs
+	node test/run-test-node.js
+```
--- test/run-test-node.js
+++ test/run-test-node.js
@@ -3,6 +3,9 @@ fs = require("fs"),
 jasmine = require("jasmine-node"),
 TerminalReporter = require("jasmine-node/lib/jasmine-node/reporter").jasmineNode.TerminalReporter;
 
+var spec_list = [];
+var executeJasmine;
+
 requirejs.config({
     nodeRequire: require,
     baseUrl: 'src/'
@@ -10,9 +13,21 @@ requirejs.config({
 
 ["test/specs"].forEach(function (path) {
     fs.readdirSync(path).forEach(function (file) {
-        requirejs(path+"/"+file);
+    	var file_name = ('../'+path+'/'+file); // '../' is because 'src/' is the root for RequireJS as configured previously
+    	var base_name = file_name.substr(0,file_name.length - 3);
+    	var ext = file_name.substr(file_name.length - 3);
+
+        //requirejs(path+"/"+file);
+        if('.js' === ext) {
+        	spec_list.push(base_name);
+        }
+        
     });
 });
 
-jasmine.getEnv().addReporter(new TerminalReporter({}));
-jasmine.getEnv().execute();
+executeJasmine = function () {
+	jasmine.getEnv().addReporter(new TerminalReporter({}));
+	jasmine.getEnv().execute();
+};
+
+requirejs.apply(requirejs, [spec_list, executeJasmine]);
