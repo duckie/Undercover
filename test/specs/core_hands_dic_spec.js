682993dba6f6d50515061cb53da72bb23b25596a
Jean-Bernard Jansen <jeanbernard.jansen@gmail.com>
2012-10-01 18:48:37 +0200

    Ergotage.

--- test/specs/core_hands_dic_spec.js
+++ test/specs/core_hands_dic_spec.js
@@ -1,5 +1,5 @@
 
-define(['underscore','undercover/core/card'],function(_, _deckmod) {
+define(['undercover/core/card'],function( _deckmod) {
 	describe('Testing card retrieval', function() {
 		
 		var deck = _deckmod.new_shuffled_deck();		
@@ -7,7 +7,25 @@ define(['underscore','undercover/core/card'],function(_, _deckmod) {
 		it('Ah value should be 14 and his color heart', function() {
 			var card = _deckmod.get_card_from_str('Ah');
 			expect(card.value()).toBe( 14 );
-			//expect( card.color() ).toBeEqualTo( _deckmod.color_codes.heart );
+			expect( card.color() ).toBe( _deckmod.color_codes.heart );
+		});
+
+		it('Js value should be 11 and his color spade', function() {
+			var card = _deckmod.get_card_from_str('Js');
+			expect(card.value()).toBe( 11 );
+			expect( card.color() ).toBe( _deckmod.color_codes.spade );
+		});
+
+		it('Qc value should be 12 and his color club', function() {
+			var card = _deckmod.get_card_from_str('Qc');
+			expect(card.value()).toBe( 12 );
+			expect( card.color() ).toBe( _deckmod.color_codes.club );
+		});
+
+		it('6d value should be 6 and his color diamond', function() {
+			var card = _deckmod.get_card_from_str('6d');
+			expect(card.value()).toBe( 6 );
+			expect( card.color() ).toBe( _deckmod.color_codes.diamond );
 		});
 	});
 
