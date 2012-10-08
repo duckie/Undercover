05e6ee88a1cb9d47dd183cf870c2181b15c81200
Jean-Bernard Jansen <jeanbernard.jansen@gmail.com>
2012-10-08 17:19:43 +0200

    Code cleaning.

--- src/undercover/core/set.js
+++ src/undercover/core/set.js
@@ -3,6 +3,9 @@ define(['underscore','./core'], function(_, _uc_){
 
 	/**
 	* Find every combination of k elements extracted from the array 'elements'
+	*
+	* The algorithm is stable, which means it keeps the original order of the elements
+	* in the given array.
 	*/
 	function find_combinations(elements, k) {
 		_uc_.assert(0 < k && k <= elements.length);
@@ -11,8 +14,6 @@ define(['underscore','./core'], function(_, _uc_){
 		var index =  0;
 		var work_elements = null;
 		var trashed_out = null;
-		var local_solutions = null;
-		var remain = null;
 
 		if(elements.length === k) {
 			result = [ _.clone(elements) ];
@@ -25,14 +26,12 @@ define(['underscore','./core'], function(_, _uc_){
 			work_elements = _.clone(elements); 
 			trashed_out = work_elements.shift();
 
-			local_solutions = find_combinations(work_elements, k-1);
-			_.each(local_solutions, function(solution) {
+			_.each(find_combinations(work_elements, k-1), function(solution) {
 				solution.unshift(trashed_out);
 				result.push(solution);
 			});
 
-			remain = find_combinations(work_elements,k);
-			_.each(remain, function(solution){
+			_.each(find_combinations(work_elements,k), function(solution){
 				result.push(solution);
 			});
 		}
