105b1fde0d7361e9b4e941ef14d6d705769721f4
Jean-Bernard Jansen <jeanbernard.jansen@gmail.com>
2012-10-11 02:44:42 +0200

    work in progress

--- src/undercover/core/game.js
+++ src/undercover/core/game.js
@@ -1,4 +1,24 @@
 define(['underscore','./core','./card', './hand'],function(_, _ucengine_ , _deck_, _hands_ ) {
-	var game_prototype = {};
+	var game_prototype = {
+		nextHand: function () {
+
+		},
+
+		bet: function (player, amount) {
+
+		},
+
+		check: function (player) {
+
+		},
+
+		fold: function (player) {
+
+		},
+
+		changeVariation: function(variation) {
+			
+		}
+	};
 	return _ucengine_.createUCObject();
 });
